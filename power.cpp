d5ae70a23b071bab2438e5c0c81a67ef50347336
alenahalm <alenahalmatova749@gmail.com>
2021-03-26 15:42:57 +0800

    Initial commit

---
+++ power.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int power(double x, int n) {
+	if (n == 0)
+		return 1;
+	else if (n % 2 == 0)
+		return power(x * x, n / 2);
+	else
+		return x* power(x, n - 1);
+}
+
+int main()
+{
+	cout << "Enter X and N" << endl;
+	int x, n;
+	cin >> x >> n;
+	cout << power(x, n) << endl;
+}
